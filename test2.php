bc1b6bfe6087afde202bc647ae73b5784f3243e9
Nick-Pieros <pierosn0@students.rowan.edu>
2017-05-03 19:37:03 -0400

    Delete test2.php

--- test2.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require_once ("functions.php");
-  require_once ("connect.php");
-  $dbh=ConnectDB();
-
-    $post_id = CreatePost($dbh, $_COOKIE['user_id'], '1111111111111111111111111111111', 'test', GetUploadId($dbh, 322));
-    echo $post_id;
- ?>
